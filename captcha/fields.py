01b7378b77e5cc6970c936df0d8d5b2650330a37
Raffaele Salmaso <raffaele@salmaso.org>
2015-07-03 23:49:50 +0200

    init captcha

---
+++ captcha/fields.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2015, Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from .utils import encode
+from .widgets import CaptchaWidget
+
+
+class CaptchaField(forms.MultiValueField):
+    widget = CaptchaWidget
+    widget_params = ["values", "operators", "question", "question_class"]
+    default_error_messages = {
+        "invalid": _("Please check your math and try again."),
+        "invalid_number": _("Enter a whole number."),
+    }
+
+    def __init__(self, *args, **kwargs):
+        widget = self.get_widget(kwargs)
+        super(CaptchaField, self).__init__(fields=(), widget=widget, *args, **kwargs)
+
+        error_messages = {
+            "invalid": self.error_messages["invalid_number"],
+        }
+        self.fields = [
+            forms.IntegerField(
+                required=False,
+                error_messages=error_messages,
+                localize=self.localize,
+            ),
+            forms.CharField(
+                required=False,
+            ),
+        ]
+
+    def compress(self, data_list):
+        if data_list:
+            answer, hashed = data_list
+            hashed_answer = encode(answer)
+            if hashed_answer != hashed:
+                raise ValidationError(self.error_messages["invalid"])
+        return None
+
+    def get_widget_params(self, kwargs):
+        params = {}
+        for key in self.widget_params:
+            if key in kwargs:
+                params[key] = kwargs.pop(key)
+        return params
+
+    def get_widget(self, kwargs):
+        params = self.get_widget_params(kwargs)
+        widget = kwargs.pop("widget", None)
+        if widget and params:
+            msg = "{} must be omitted when widget is provided for {}.".format(
+                " and ".join(list(params)),
+                self.__class__.__name__,
+            )
+            raise TypeError(msg)
+        elif not widget:
+            widget = self.widget(**params)
+        return widget
---
+++ captcha/widgets.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2015, Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from django import forms
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+from .utils import get_new_values, encode
+from . import settings
+
+
+OPERATORS = list(settings.OPERATIONS)
+
+
+class CaptchaWidget(forms.MultiWidget):
+    output = "<span class=\"{question_class}\">{question}</span>{text_input}{hidden_input}"
+    values = settings.NUMBERS
+    operators = OPERATORS
+    question = _("Are you human? What is {x} {operator} {y}? ")
+    question_class = "captcha-question"
+
+    def __init__(self, values=None, operators=None, question=None, question_class=None, attrs=None):
+        self.values = values or self.values
+        self.operators = operators or self.operators
+        self.question_class = question_class or self.question_class
+        self.question_tmpl = question or self.question
+        widget_attrs = {"size": "5"}
+        widget_attrs.update(attrs or {})
+        widgets = [
+            forms.TextInput(attrs=widget_attrs),
+            forms.HiddenInput()
+        ]
+        super(CaptchaWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        return [None, None]
+
+    def format_output(self, rendered_widgets):
+        text_input, hidden_input = rendered_widgets
+
+        output = self.output.format(
+            question_class=self.question_class,
+            question=self.question_html,
+            text_input=text_input,
+            hidden_input=hidden_input,
+        )
+
+        return mark_safe(output)
+
+    def render_question(self, x, y, operator):
+        operator = "&times;" if operator == "*" else operator
+        question = self.question_tmpl.format(
+            x=x,
+            operator=operator,
+            y=y,
+        )
+        return mark_safe(question)
+
+    def render(self, name, value, attrs=None):
+        x, y, operator, answer = get_new_values(self.values, self.operators)
+        self.question_html = self.render_question(x, y, operator)
+        value = ["", encode(answer)]
+        return super(CaptchaWidget, self).render(name, value, attrs=attrs)
